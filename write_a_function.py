c6bd9d4c65e27432021295a370dac000927c9118
Govind <govindajithkumar@gmail.com>
2020-09-29 18:23:30 -0400

    new commit

--- write_a_function.py
+++ write_a_function.py
@@ -1,9 +1,14 @@
 def is_leap(year):
     leap = False
-
-    # Write your logic here
-
+    if (year % 4 == 0):
+        if (year % 100 == 0):
+            if (year % 400 == 0):
+                leap = True
+                return leap
+            leap = False
+            return leap
+        leap = True
+        return leap
     return leap
-
-
 year = int(input())
+print(is_leap(year))
